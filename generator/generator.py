fcb338cc76cca68c8e3222d8566571d791607ae5
Majeux <manblankestijn@gmail.com>
2020-10-31 13:21:51 +0100

    met normal, moet nog testen

--- generator/generator.py
+++ generator/generator.py
@@ -1,28 +1,27 @@
+from scipy.stats import truncnorm
 from random import randrange
 from time import sleep
 from queue import Full as queueFullError
+from scipy.stats import truncnorm
 import ntplib
+import matplotlib.pyplot as plt
 
 # .py
 from our_ntp import getLocalTime
 from benchmark_driver import STOP_TOKEN
 
-USER_RANGE  = 1000 #idk
 GEM_RANGE   = 8
 PRICE_RANGE = 5
 
 PUT_TIMEOUT = 0
 
-def gen_ad(time_client):
-    return (
-        randrange(USER_RANGE),
-        randrange(GEM_RANGE),
-        getLocalTime(time_client)
-    )
+def rand_normal(mean, sd, low, mean):
+        return truncnorm(
+                    (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
-def gen_purchase(time_client):
+def gen_purchase(time_client=None):
     purchase = (
-        randrange(GEM_RANGE),
+        rand_normal(GEM_RANGE/2, GEM_RANGE/4, 0, GEM_RANGE),
         randrange(PRICE_RANGE),
         getLocalTime(time_client)
     )
@@ -61,11 +60,10 @@ def purchase_generator(q, error, time_client, id, rate, budget):
         sleep(max(0, 1/rate - diff))
 
 if __name__ == "__main__":
-    print("Test purchase_generator")
-    for i in range(10):
-        print(gen_purchase(ntplib.NTPClient()))
-
     print("__________________\nTest ad_generator")
 
     for i in range(10):
-        print(gen_ad(ntplib.NTPClient()))
+        print(gen_purchase())
+
+    # fig, ax = plt.subplots(3, sharex=True)
+    # ax[0].hist(X1.rvs(10000), normed=True)
