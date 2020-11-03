196ca532919fc4e7945878bc5a7cc72a7ccd93e8
rintse <r1ntse.a@gmail.com>
2020-11-03 21:13:16 +0100

    Beter commentaar in run_bench script

--- deployment/run_bench.py
+++ deployment/run_bench.py
@@ -1,43 +1,44 @@
 #!/bin/python3
-
 import os
 import sys
 import subprocess
 import time
 from deploy import deploy_all
 
-MAX_WAIT_TIME = 920
-POLLING_INTERVAL = 2
+# PARAMETERS
+MAX_WAIT_TIME = 920 # Maximum amount of time spent waiting for reservation
+POLLING_INTERVAL = 2 # Amount of seconds between each status check
+BASE_NODES = 3 # Number of nodes needed besides the workers
 
-# COLUMN INDEXES OF RESERVATION INFO
+# COLUMN INDICES OF RESERVATION INFO
 ID_IDX=0
 NODES_IDX=8
 STATUS_IDX=6
 
+# Returns the reservation info as a list
 def get_reservation_info():
     result = subprocess.check_output("preserve -llist | grep ddps2016", shell=True).decode("utf-8")
     if result.count("\n") > 1:
         raise Exception("Multiple reservations found")
     return result.split()
 
+# Cancels a reservation with id reservation_id
 def cancel_reservation(reservation_id):
     print("Cancelling reservation")
     os.system("preserve -c " + reservation_id)
 
-	
+# Read command line args
+# Mandatory arguments: num_workers, gen_rate
 if len(sys.argv) < 3:
     print("You must supply a number of worker nodes, and a data generation speed")
     exit()
-
-# Read command line args
 num_workers = int(sys.argv[1])
-
 gen_rate = int(sys.argv[2])
+
 print("Benchmarking generation rate ", gen_rate, " on ", num_workers, " workers")
 
 # Reserve the nodes
-os.system("preserve -# " + str(num_workers + 3) + " -t 00:15:00")
-time.sleep(1)
+os.system("preserve -# " + str(num_workers + BASE_NODES) + " -t 00:15:00")
 
 # Get reservation info
 reservation = get_reservation_info()
@@ -48,6 +49,7 @@ reserved_nodes = reservation[NODES_IDX:]
 # Continuously check whether the nodes are available
 cur_waiting_time=0
 while reservation_status != "R" and reserved_nodes is not []:
+    # Update reservation info
     reservation = get_reservation_info()
     reservation_status = reservation[STATUS_IDX]
     reserved_nodes = reservation[NODES_IDX:]
@@ -63,6 +65,4 @@ while reservation_status != "R" and reserved_nodes is not []:
 
 # If we've gotten here, the reservation is ready
 print("Got reservation on nodes: ", reserved_nodes)
-print("Deploying cluster.")
-
 deploy_all(reserved_nodes, gen_rate, reservation_id)
